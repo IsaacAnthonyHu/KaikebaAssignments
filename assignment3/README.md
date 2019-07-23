604287540be942120217c7b8441fc3bb6813f032
IsaacAnthonyHu <IsaacAnthonyHu@users.noreply.github.com>
2019-07-23 17:24:08 +0800

    20190723 commit
    Add linear regression
    update README

---
+++ assignment3/README.md
@@ -0,0 +1,60 @@
+## Rewrite Linear Regression in Python
+
+try pseudocode with Linear Regression:
+
+```pseudocode
+Given:
+	eta - learning rate
+	data - a set of data points % e.g. [(x,y),(a,b)...]
+Return:
+	theta0 - a parameter of model (h = theta1 * x + theta0)
+	theta1 - a parameter of model (h = theta1 * x + theta0)
+	loss - result of loss function under theta0 and theta1
+
+theta0 = 0
+theta1 = 0
+% set default theta values
+x_list = a set of x axis value from data
+y_list = a set of y axis value from data
+sample_numbers = the number of all samples
+
+theta0 = theta0 - eta * (sum of all points in (theta1*x + theta0 + y))
+theta1 = theta1 - eta * (sum of all points in (theta1*x + theta0 + y)*x)
+
+```
+
+try 不出来，直接写代码吧
+
+---
+
+代码中定义了以下几个函数：
+
+#### seperate_xy()
+
+将数据点集拆分为x轴与y轴的列表
+
+#### step_gradient()
+
+计算每一步的梯度值
+
+#### evaluate_lost()
+
+计算每一步损失函数的值
+
+#### train()
+
+输入原数据集，learning_rate, 最大迭代次数，损失函数下限
+
+将输出每一步的各θ与损失函数的值，超过最大迭代次数或损失函数到达下限都将结束循环
+
+#### gen_test_data_set()
+
+测试数据集
+
+#### run()
+
+结合以上函数，进行处理
+
+## Rewrite Logistic Regression in Python
+
+## Mathematics
---
+++ assignment3/linear_regression.py
@@ -0,0 +1,89 @@
+import random
+import numpy as np
+
+def predict_y(theta0, theta1, x):
+
+    pred_y = theta1 * x + theta0
+
+    return pred_y
+
+
+def seperate_xy(data):
+
+    x_list = np.array([coord[0] for coord in data])
+    y_list = np.array([coord[1] for coord in data])
+
+    return x_list, y_list
+
+
+def step_gradient(x_list, y_list, eta, theta0, theta1):
+
+    d_theta0 = (sum(theta1 * x_list) + len(x_list) * theta0 - sum(y_list)) / len(x_list)
+
+    d_theta1 = (sum(theta1 * x_list ** 2) + sum(theta0 * x_list) - sum(x_list * y_list)) / len(x_list)
+
+    return d_theta0, d_theta1
+
+def evaluate_lost(x_list, y_list, theta0, theta1):
+
+    lost = 0
+
+    for x in range(len(x_list)):
+
+        lost += (theta1 * x_list[x] + theta0 - y_list[x])**2
+
+    return lost/(2*len(x_list))
+
+
+
+def train(data, eta, max_iter, thres=0.001):
+
+    x_list, y_list = seperate_xy(data)
+
+    num_coord = len(data)
+
+    theta0, theta1 = 0, 0
+
+    lost_history = []
+
+    for i in range(max_iter):
+
+        d_theta0, d_theta1 = step_gradient(x_list, y_list, eta, theta0, theta1)
+
+        theta0 -= eta * d_theta0
+        
+        theta1 -= eta * d_theta1
+
+        lost = evaluate_lost(x_list, y_list, theta0, theta1)
+        
+        lost_history.append(lost)
+
+        print('''
+        -----
+        theta0:  %r
+        theta1:  %r
+        lost:    %r
+        -----
+        '''%(theta0, theta1, lost))
+
+        if lost <= thres:
+            break
+
+    return theta0, theta1, lost_history
+
+def gen_test_data_set():
+
+    data_points = [(2,3), (-1,1), (3,4), (5,5), (-6,-2)]
+
+    return data_points
+
+def run():
+
+    data = gen_test_data_set()
+
+    theta0, theta1, lost_history = train(data, 0.001, 10000)
+
+    #print(lost_history)
+
+if __name__ == '__main__':
+    run()
